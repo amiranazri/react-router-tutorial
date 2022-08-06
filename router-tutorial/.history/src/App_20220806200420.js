e2e1e17b508f326e1987b550def1f852f5966bc0
Amira Nazri <abbnazri@gmail.com>
2022-08-06 20:10:56 +0800

    Created new project using bundler, connected the URL, added some test links.

---
+++ router-tutorial/.history/src/App_20220806200420.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Bookkeeper!</h1>
+    <div>
+    </div>      
+    </div>
+  );
+}
+
+export default App;
---
+++ router-tutorial/.history/src/App_20220806200919.js
@@ -0,0 +1,20 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Bookkeeper!</h1>
+      <nav
+        style={{
+          borderBottom: "solid 1px",
+          paddingBottom: "1rem",
+        }}
+      >
+        <Link to="/invoices">Invoices</Link> |{" "}
+        <Link to="/expenses">Expenses</Link>
+      </nav>
+    </div>
+  );
+}
+
+export default App;
